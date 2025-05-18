08a95ef9f053a9ca7a467a001cb70dd40abf0f4a
Daniel <danielpbea@gmail.com>
2025-05-18 14:14:50 +0300

    Fixed quitting issue

--- Ex02/Ex02/Ui.cs
+++ Ex02/Ex02/Ui.cs
@@ -90,7 +90,7 @@ namespace UeserInterface
 
             }
 
-            if (quitting)
+            if (!quitting)
             {
                 printGameResultScreen();
                 promptUserForRetry();
